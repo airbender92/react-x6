865b79caf6ab3bc7eaa021064a92e5660fed1814
airbender92 <yambo1992@outlook.com>
2025-04-11 17:16:07 +0800

    xx

--- src/components/DocumentEditor/index.jsx
+++ src/components/DocumentEditor/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import domToImage from 'dom-to-image';
+import HatechEditor from '../../utils/HatechEditor';
 import OrgTreeComponent from './OrgTreeComponent';
+import BackToTop from './BackToTop';
 
 const DocumentEditor = () => {
     return (
